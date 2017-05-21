94ab9747ab6dd35f4ae9428d3350e18e2aab4292
Matthias Guenther <matthias@wikimatze.de>
2017-05-21 06:49:09 +0200

    Add posts controller

--- admin/app.rb
+++ admin/app.rb
@@ -35,6 +35,7 @@ module BlogTutorial
     end
 
     access_control.roles_for :admin do |role|
+      role.project_module :posts, '/posts'
       role.project_module :accounts, '/accounts'
     end
 
