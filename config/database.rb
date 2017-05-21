baf030172fb0c94206a1fa313a566ca77d01ff4d
Matthias Guenther <matthias@wikimatze.de>
2017-05-21 19:23:47 +0200

    Add db settings for heroku

--- config/database.rb
+++ config/database.rb
@@ -1,7 +1,6 @@
-Sequel::Model.plugin(:schema)
 Sequel::Model.raise_on_save_failure = false # Do not throw exceptions on failure
 Sequel::Model.db = case Padrino.env
   when :development then Sequel.connect("sqlite://db/blog_tutorial_development.db", :loggers => [logger])
-  when :production  then Sequel.connect("postgres://ebwfzzgzxiwnhq:c91612b955891736ee7e222924d02db27bfe19b67098db8fa8cdd5f3675feef6@ec2-50-19-95-47.compute-1.amazonaws.com:5432/dd48t9p70h3pvo",  :loggers => [logger])
+  when :production  then Sequel.connect("postgres://mrmjyshxlocriz:c6315a3f0319c08e9fb895514fe12f98ec6470047aec3071223ad9a363cd7c7c@ec2-54-247-166-129.eu-west-1.compute.amazonaws.com:5432/dajt96oh1akfuu", :loggers => [logger])
   when :test        then Sequel.connect("sqlite://db/blog_tutorial_test.db",        :loggers => [logger])
 end
