8d198a8cc0b3863a62227217286ee92c5508fcd9
Matthias Guenther <matthias@wikimatze.de>
2017-05-26 20:20:49 +0200

    Code-review: drop class definitions in migrations

--- db/migrate/003_add_account_to_post.rb
+++ db/migrate/003_add_account_to_post.rb
@@ -1,10 +1,4 @@
 Sequel.migration do
-    # Stub Models
-  class Account < Sequel::Model; end
-  class Post < Sequel::Model
-    many_to_one :account
-  end
-
   up do
     alter_table :posts do
       add_column :account_id, Integer
--- db/migrate/004_migrate_existing_posts_to_first_account.rb
+++ db/migrate/004_migrate_existing_posts_to_first_account.rb
@@ -1,9 +1,4 @@
 Sequel.migration do
-  class Account < Sequel::Model; end
-  class Post < Sequel::Model
-    many_to_one :account
-  end
-
   up do
     first_account_id = from(:accounts).get(:id)
 
